630abe8fbbd4ea469ae754d1c7a0d6c851f5c3b4
Lim Seo Yeon <sora37718@gmail.com>
2026-09-18 20:23:00 +0900

    03-02

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
-    int input_int;
-    float input_float;
-
-    printf("enter an integer: ");
-    scanf("%d", &input_int);
-
-    printf("enter a float: ");
-    scanf("%f", &input_float);
-
-    printf("integer: %d, float: %f\n", input_int, input_float);
+int main(void)
+{
+    char c;
+    int i;
+    
+    printf("input a number :");
+    scanf("%c", &c);
+    
+    i = c-'0';
+    printf("The input number is %i\n", i);
 
     return 0;
 }
